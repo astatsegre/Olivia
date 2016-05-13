ddaf25247aa755e44263b3427fd8867820ae9cb8
Stanislav <novitckas@gmail.com>
2016-05-13 11:14:48 +0300

    added js

---
+++ JS/script.js
@@ -0,0 +1,9 @@
+var formFeedback = document.forms.feedback
+
+formFeedback.first_name.value = "First Name";
+formFeedback.last_name.value = "Last Name";
+formFeedback.email.value = "Email";
+formFeedback.tel.value = "Tel. Number";
+formFeedback.comments.value = "Comments"
+
+alert (navigator.userAgent);
