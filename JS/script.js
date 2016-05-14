4b51e4ffc2c1ce2825ed0501163f4b4e3e4dad58
Stanislav <novitckas@gmail.com>
2016-05-15 01:08:02 +0300

    fixed some bugs

--- JS/script.js
+++ JS/script.js
@@ -1,9 +1,10 @@
 var formFeedback = document.forms.feedback
 
-formFeedback.first_name.value = "First Name";
-formFeedback.last_name.value = "Last Name";
-formFeedback.email.value = "Email";
-formFeedback.tel.value = "Tel. Number";
-formFeedback.comments.value = "Comments"
+if (bowser.msie && bowser.version <= 9) {
+    formFeedback.first_name.value = "First Name";
+    formFeedback.last_name.value = "Last Name";
+    formFeedback.email.value = "Email";
+    formFeedback.tel.value = "Tel. Number";
+    formFeedback.comments.value = "Comments"
+}
 
-alert (navigator.userAgent);
