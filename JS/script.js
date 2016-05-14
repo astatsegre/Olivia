63aeb4355d069c5681e164dcff13db0b8dc20eaa
astatsegre <astatsegre@gmail.com>
2016-05-14 18:05:03 +0300

    Revert "added js"
    This reverts commit ddaf25247aa755e44263b3427fd8867820ae9cb8.

--- JS/script.js
+++
@@ -1,9 +0,0 @@
-var formFeedback = document.forms.feedback
-
-formFeedback.first_name.value = "First Name";
-formFeedback.last_name.value = "Last Name";
-formFeedback.email.value = "Email";
-formFeedback.tel.value = "Tel. Number";
-formFeedback.comments.value = "Comments"
-
-alert (navigator.userAgent);
